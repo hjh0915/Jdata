775916277a931ef0194da8af4911385e0d6a2230
hjh0915 <623299157@qq.com>
2019-08-08 20:43:52 +0800

    分析并查询电影影评情况

--- src/main/java/com/java/data/MyRunner.java
+++ src/main/java/com/java/data/MyRunner.java
@@ -21,94 +21,94 @@ public class MyRunner implements CommandLineRunner {
     JdbcTemplate jdbcTemplate;
 
     public void run(String... args) throws Exception {
-        // getUsers();
-        // getMovies();
+        getUsers();
+        getMovies();
         getRatings();
     }
 
-    // public void getUsers() throws IOException {
+    public void getUsers() throws IOException {
 
-    //     List<String[]> rows = new ArrayList<>();
+        List<String[]> rows = new ArrayList<>();
 
-    //     String fileName = "users.dat";
-    //     InputStream in = MyRunner.class.getClassLoader().getResourceAsStream(fileName);
-    //     String line;
+        String fileName = "users.dat";
+        InputStream in = MyRunner.class.getClassLoader().getResourceAsStream(fileName);
+        String line;
 
-    //     BufferedReader br = new BufferedReader(new InputStreamReader(in));
-    //     int i = 1;
+        BufferedReader br = new BufferedReader(new InputStreamReader(in));
+        int i = 1;
 
-    //     while ((line = br.readLine()) != null) {
-    //         String[] words = line.split("::");
-    //         if ( i % 2000 == 0 ) {
-    //             writeUsers(rows);
-    //             rows = new ArrayList<>();
-    //         }
-    //         rows.add(words);
+        while ((line = br.readLine()) != null) {
+            String[] words = line.split("::");
+            if ( i % 2000 == 0 ) {
+                writeUsers(rows);
+                rows = new ArrayList<>();
+            }
+            rows.add(words);
 
-    //         i++;
-    //     }
+            i++;
+        }
 
-    //     writeUsers(rows);
+        writeUsers(rows);
 
-    // }
+    }
 
-    // public void writeUsers(List<String[]> rows) throws IOException {
-    //     String sql = "insert into users (user_id, gender, age, occupation, zip) values (?, ?, ?, ?, ?)";
-    //     jdbcTemplate.batchUpdate(sql, new BatchPreparedStatementSetter() {
-    //         public int getBatchSize() {
-    //             return rows.size();
-    //         }
-    //         public void setValues(PreparedStatement ps, int i)throws SQLException {
-    //             String[] words = rows.get(i);
-    //             ps.setInt(1, Integer.parseInt(words[0]));
-    //             ps.setString(2, words[1]);
-    //             ps.setInt(3, Integer.parseInt(words[2]));
-    //             ps.setInt(4, Integer.parseInt(words[3]));
-    //             ps.setString(5, words[4]);
-    //         }
-    //     }); 
-    // }
+    public void writeUsers(List<String[]> rows) throws IOException {
+        String sql = "insert into users (user_id, gender, age, occupation, zip) values (?, ?, ?, ?, ?)";
+        jdbcTemplate.batchUpdate(sql, new BatchPreparedStatementSetter() {
+            public int getBatchSize() {
+                return rows.size();
+            }
+            public void setValues(PreparedStatement ps, int i)throws SQLException {
+                String[] words = rows.get(i);
+                ps.setInt(1, Integer.parseInt(words[0]));
+                ps.setString(2, words[1]);
+                ps.setInt(3, Integer.parseInt(words[2]));
+                ps.setInt(4, Integer.parseInt(words[3]));
+                ps.setString(5, words[4]);
+            }
+        }); 
+    }
 
-    // public void getMovies() throws IOException {
+    public void getMovies() throws IOException {
 
-    //     List<String[]> rows = new ArrayList<>();
+        List<String[]> rows = new ArrayList<>();
 
-    //     String fileName = "movies.dat";
-    //     InputStream in = MyRunner.class.getClassLoader().getResourceAsStream(fileName);
-    //     String line;
+        String fileName = "movies.dat";
+        InputStream in = MyRunner.class.getClassLoader().getResourceAsStream(fileName);
+        String line;
 
-    //     BufferedReader br = new BufferedReader(new InputStreamReader(in));
-    //     int i = 1;
+        BufferedReader br = new BufferedReader(new InputStreamReader(in));
+        int i = 1;
 
-    //     while ((line = br.readLine()) != null) {
-    //         String[] words = line.split("::");
-    //         if ( i % 2000 == 0 ) {
-    //             writeMovies(rows);
-    //             rows = new ArrayList<>();
-    //         }
-    //         rows.add(words);
+        while ((line = br.readLine()) != null) {
+            String[] words = line.split("::");
+            if ( i % 2000 == 0 ) {
+                writeMovies(rows);
+                rows = new ArrayList<>();
+            }
+            rows.add(words);
 
-    //         i++;
-    //     }
+            i++;
+        }
 
-    //     writeMovies(rows);
+        writeMovies(rows);
 
-    // }
+    }
 
-    // public void writeMovies(List<String[]> rows) throws IOException {
-    //     String sql = "insert into movies (movie_id, title, genres) values (?, ?, ?)";
-    //     jdbcTemplate.batchUpdate(sql, new BatchPreparedStatementSetter() {
-    //         public int getBatchSize() {
-    //             return rows.size();
-    //         }
-    //         public void setValues(PreparedStatement ps, int i)throws SQLException {
-    //             String[] words = rows.get(i);
-    //             ps.setInt(1, Integer.parseInt(words[0]));
-    //             ps.setString(2, words[1]);
-    //             ps.setString(3, words[2]);
-    //         }
-    //     }); 
-    // }
+    public void writeMovies(List<String[]> rows) throws IOException {
+        String sql = "insert into movies (movie_id, title, genres) values (?, ?, ?)";
+        jdbcTemplate.batchUpdate(sql, new BatchPreparedStatementSetter() {
+            public int getBatchSize() {
+                return rows.size();
+            }
+            public void setValues(PreparedStatement ps, int i)throws SQLException {
+                String[] words = rows.get(i);
+                ps.setInt(1, Integer.parseInt(words[0]));
+                ps.setString(2, words[1]);
+                ps.setString(3, words[2]);
+            }
+        }); 
+    }
 
     public void getRatings() throws IOException {
 
---
+++ src/main/resources/query.sql
@@ -0,0 +1,126 @@
+select * from users limit 5;
+
+select * from users u inner join rates r on u.user_id=r.user_id inner join movies m on r.movie_id=m.movie_id;
+
+/**
+按性别分级的每部电影的平均电影评分
+*/
+select t.movie_id, t.title, sum(t.female) as F, sum(t.male) as M 
+from (
+	select m.movie_id, m.title, 
+           avg(case when (u.gender = 'F') then  rating else 0 end) as female,
+	       avg(case when(u.gender = 'M') then rating else 0 end) as male
+	from rates r, users u, movies m
+	where r.movie_id=m.movie_id and r.user_id=u.user_id
+	group by m.movie_id, m.title, u.gender
+) t
+group by t.movie_id, t.title;
+
+/**
+筛选出大于250个有评分的电影
+*/
+select m.movie_id, m.title, count(*) as cnt 
+from movies m, rates r
+where m.movie_id=r.movie_id
+group by m.movie_id, m.title
+having count(*)>250;
+
+/**
+筛选出大于250个有评分的电影，按性别分级的每部电影的平均电影评分
+*/
+select p1.* 
+from (
+    select t.movie_id, t.title, sum(t.female) as F, sum(t.male) as M 
+    from (
+	    select m.movie_id, m.title, 
+            avg(case when (u.gender = 'F') then  rating else 0 end) as female,
+	        avg(case when(u.gender = 'M') then rating else 0 end) as male
+	    from rates r, users u, movies m
+	    where r.movie_id=m.movie_id and r.user_id=u.user_id
+	    group by m.movie_id, m.title, u.gender
+    ) t
+    group by t.movie_id, t.title
+) p1, (
+    select m.movie_id, m.title, count(*) as cnt 
+    from movies m, rates r
+	where m.movie_id=r.movie_id
+	group by m.movie_id, m.title
+	having count(*)>250
+) p2
+where p1.movie_id=p2.movie_id;
+
+/**
+筛选出大于250个有评分的电影，按性别分级的每部电影的平均电影评分，并按照女性观众电影评分排行榜
+*/
+select p1.* 
+from (
+    select t.movie_id, t.title, sum(t.female) as F, sum(t.male) as M 
+    from (
+	    select m.movie_id, m.title, 
+            avg(case when (u.gender = 'F') then  rating else 0 end) as female,
+	        avg(case when(u.gender = 'M') then rating else 0 end) as male
+	    from rates r, users u, movies m
+	    where r.movie_id=m.movie_id and r.user_id=u.user_id
+	    group by m.movie_id, m.title, u.gender
+    ) t
+    group by t.movie_id, t.title
+    ) p1, (
+        select m.movie_id, m.title, count(*) as cnt from movies m, rates r
+	    where m.movie_id=r.movie_id
+	    group by m.movie_id, m.title
+	    having count(*)>250
+    ) p2
+where p1.movie_id=p2.movie_id
+order by p1.F desc;
+
+/**
+筛选出大于250个有评分的电影，按性别分级的每部电影的平均电影评分
+男性首选电影，女性评分不高
+*/
+select n.*, (n.M-n.F) as diff 
+from (
+    select p1.* from (select t.movie_id, t.title, sum(t.female) as F, sum(t.male) as M 
+    from (
+	    select m.movie_id, m.title, 
+            avg(case when (u.gender = 'F') then  rating else 0 end) as female,
+	        avg(case when(u.gender = 'M') then rating else 0 end) as male
+	    from rates r, users u, movies m
+	    where r.movie_id=m.movie_id and r.user_id=u.user_id
+	    group by m.movie_id, m.title, u.gender
+    ) t
+    group by t.movie_id, t.title
+    ) p1, (
+        select m.movie_id, m.title, count(*) as cnt from movies m, rates r
+	    where m.movie_id=r.movie_id
+	    group by m.movie_id, m.title
+	    having count(*)>250
+    ) p2
+where p1.movie_id=p2.movie_id) n
+order by diff asc;
+
+/**
+筛选出大于250个有评分的电影，按性别分级的每部电影的平均电影评分
+女性首选电影，男性评分不高
+*/
+select n.*, (n.F-n.M) as diff 
+from (
+    select p1.* 
+    from (
+        select t.movie_id, t.title, sum(t.female) as F, sum(t.male) as M 
+        from (
+	        select m.movie_id, m.title, 
+                avg(case when (u.gender = 'F') then  rating else 0 end) as female,
+	            avg(case when(u.gender = 'M') then rating else 0 end) as male
+	        from rates r, users u, movies m
+	        where r.movie_id=m.movie_id and r.user_id=u.user_id
+	        group by m.movie_id, m.title, u.gender
+        ) t
+        group by t.movie_id, t.title
+    ) p1, (
+        select m.movie_id, m.title, count(*) as cnt from movies m, rates r
+	    where m.movie_id=r.movie_id
+	    group by m.movie_id, m.title
+	    having count(*)>250
+    ) p2
+where p1.movie_id=p2.movie_id) n
+order by diff asc;
